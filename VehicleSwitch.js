6583f36eabadfb257298ec307fec25a444a36a6c
fuobaasan <99116156+fuobaasan@users.noreply.github.com>
2022-02-20 23:38:12 +0900

    Update VehicleSwitch.js

--- VehicleSwitch.js
+++ VehicleSwitch.js
@@ -4,13 +4,13 @@
 
 /*:ja
  * @target MZ
- * @plugindesc 11のスイッチオンのとき乗り物のれない＆そのときスイッチ１２オン
+ * @plugindesc 11のスイッチオンのとき乗り物のれない＆そのときスイッチ１２オン rev02
  * @author fuobaa
  *
  * @help VehicleSwitch.js
  *
  * 必要に応じてソースコード上のスイッチ 11 12を別のスイッチに直接書き換えて下さい
- *
+ * ( $gameSwitches.value(11) と $gameSwitches.setValue(12,true) の所)
  * Copyright (c) 2022 fuobaa
  * Released under the MIT license
  * https://opensource.org/licenses/mit-license.php
@@ -23,8 +23,19 @@
 
 var f_Game_Player_getOnVehicle = Game_Player.prototype.getOnVehicle;
 Game_Player.prototype.getOnVehicle = function() {
+    const direction = this.direction();
+    const x1 = this.x;
+    const y1 = this.y;
+    const x2 = $gameMap.roundXWithDirection(x1, direction);
+    const y2 = $gameMap.roundYWithDirection(y1, direction);
 if ($gameSwitches.value(11)){
+    if ($gameMap.airship().pos(x1, y1)) {
  $gameSwitches.setValue(12,true);
+    } else if ($gameMap.ship().pos(x2, y2)) {
+ $gameSwitches.setValue(12,true);
+    } else if ($gameMap.boat().pos(x2, y2)) {
+ $gameSwitches.setValue(12,true);
+    }
  return false;
 }
 return f_Game_Player_getOnVehicle.call(this);
